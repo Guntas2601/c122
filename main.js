d06893565af681430ba08663f8dc8ad0ececb029
Guntas2601 <97093375+Guntas2601@users.noreply.github.com>
2023-10-11 15:56:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,5 +2,21 @@ function setup(){
 
 canvas=createCanvas(400,400);
 canvas.center();
+video=createCapture(VIDEO);
+video.size(550,500);
+PoseNet=ml5.poseNet(video,modelloaded)
+PoseNet.on("pose",gotposes);
 
+
+}
+function modelloaded(){
+
+console.log ("posenet is initialised");
+
+}
+function gotposes(results){
+    if(results.length > 0);
+    {
+        console.log (results);
+    }
 }
